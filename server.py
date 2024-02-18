94a3da18d96b252c3652643953204bc85a0914cc
temrer <temrer14915@gmail.com>
2024-02-18 02:23:14 +0200

    running local_server with multiple clients

--- server.py
+++ server.py
@@ -1,12 +1,11 @@
 import socket
 import threading
-import asyncio
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 20048  # Port to listen on (non-privileged ports are > 1023)
-thread_runLobby 
 
-def handleConnection(conn, addr):
+def handleConnection(conn_data):
+	conn, addr = conn_data[0], conn_data[1]
 	print(f"connection has been established at address {addr[0]}, port {addr[1]}")
 	userNotExited = True
 	while userNotExited:
@@ -21,28 +20,20 @@ def handleConnection(conn, addr):
 
 
 def runLobby():
-	global thread_runLobbby
 	continueCondition = True
 	backgroundTasks = set()
 	while continueCondition:
 		print("awaiting connection")
 		conn, addr = s.accept()
-		print("connection inclming", addr)
-		#new_task = asyncio.to_thread(handleConnection(conn, addr))
-		#backgroundTasks.add(new_task)
-		thread_handleConnection = threading.Thread(target=handleConnection, args={conn, addr})
+		print("connection incoming", addr)
+		thread_handleConnection = threading.Thread(target=handleConnection, args={(conn, addr)})
 		thread_handleConnection.start()
-		thread_handleConnection.join()
-		thread_runLobby.join()
 		
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-	global thread_runLobby
 	s.bind((HOST, PORT))
 	s.listen()
 	print("initializing server")
-	thread_runLobby = threading.Thread(target=runLobby)
-	thread_runLobby.start()
-	thread_runLobby.join()
+	runLobby()
 
